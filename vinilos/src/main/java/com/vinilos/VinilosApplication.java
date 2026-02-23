abc50959ac4e839ccec6aa491ebc8dea1cb97bc4
Diego S. <guipas.diego@gmail.com>
2026-02-23 14:55:06 +0100

    Primera semana

---
+++ vinilos/src/main/java/com/vinilos/VinilosApplication.java
@@ -0,0 +1,13 @@
+package com.vinilos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinilosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinilosApplication.class, args);
+	}
+
+}
